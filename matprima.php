bba1df60be2913bb89b5bd6063b65a111142705f
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2021-11-26 21:46:54 -0300

    Agregado pedidos y cargar pedidos como opciones

--- matprima.php
+++ matprima.php
@@ -57,8 +57,8 @@ require "database.php";
                                 Ventas
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="./cargarpedido.php">Cargar Pedido</a></li>
+                                <li><a class="dropdown-item" href="#">Pedidos</a></li>
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
